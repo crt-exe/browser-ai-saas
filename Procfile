9514b0f17cc7c99c377f77e6c6f5d8a8abf99c1c
crt-exe <220395358+crt-exe@users.noreply.github.com>
2025-12-17 00:44:23 -0500

    Initial commit: SaaS adaptation of browser-use/web-ui

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python webui.py --ip 0.0.0.0 --port $PORT
---
+++ webui.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+load_dotenv()
+import argparse
+import os
+from src.webui.interface import theme_map, create_ui
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Gradio WebUI for Browser Agent")
+    parser.add_argument("--ip", type=str, default="0.0.0.0", help="IP address to bind to")
+    parser.add_argument("--port", type=int, default=os.environ.get("PORT", 7788), help="Port to listen on")
+    parser.add_argument("--theme", type=str, default="Ocean", choices=theme_map.keys(), help="Theme to use for the UI")
+    args = parser.parse_args()
+
+    demo = create_ui(theme_name=args.theme)
+    demo.queue().launch(server_name=args.ip, server_port=args.port)
+
+
+if __name__ == '__main__':
+    main()
